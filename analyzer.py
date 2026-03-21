33d8f27ef6861151be035819303ba6d3c28129ea
eje019 <emricndayake@gmail.com>
2026-03-21 16:42:25 +0100

    Feat : Definition du niveau pas l'utilisateur avec TOUS les niveaux pas defaut

--- analyzer.py
+++ analyzer.py
@@ -5,5 +5,6 @@ print("Bonjour je suis l'analyseur de LOGS")
 analyseur = argparse.ArgumentParser()
 analyseur.add_argument("--source")
 args = analyseur.parse_args()
+analyseur.add_argument("--niveau", default="ALL")
 
 print("Le dossier a analyser est :", args.source, required=True)
