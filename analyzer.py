20eaf62616008d23fbad2994282d59de8ef9f600
eje019 <emricndayake@gmail.com>
2026-03-22 18:20:37 +0100

    Tests

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 
 print("Bonjour je suis l'analyseur de LOGS")
 
@@ -7,4 +8,7 @@ analyseur.add_argument("--source")
 args = analyseur.parse_args()
 analyseur.add_argument("--niveau", default="ALL")
 
-print("Le dossier a analyser est :", args.source, required=True)
+# print("Le dossier a analyser est :", args.source, " au niveau :" ,args.niveau ,required=True)
+
+print("Dossier : ", args.source)
+print("Dossier : ", args.niveau)
