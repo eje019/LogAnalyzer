65618a0dcb51c9d01f1e5273116a95db661d6bad
eje019 <emricndayake@gmail.com>
2026-03-22 19:15:45 +0100

    Feat : Ajout de la lecture des fichiers logs et comptage des lignes

--- analyzer.py
+++ analyzer.py
@@ -17,3 +17,17 @@ fichiers_log = glob.glob(args.source + "/*.log")
 print("Dossier : ", args.source)
 print("Niveau : ", args.niveau)
 print("Fichiers trouvés :", fichiers_log)
+
+
+total_lignes = 0
+
+# lecture de chaque fichiers
+for fichier in fichiers_log:
+    # ouverture du fichier en mode lecturee seule
+    with open(fichier, "r", encoding="utf-8") as f:
+        lignes = f.readlines() #on lit toutes les lignes du fichier
+        total_lignes = total_lignes + len(lignes) #on ajoute le nombre de lignes au total
+
+        print(fichier, "contient", len(lignes), "lignes") #on dit ce fichier contient combien de lignes d'abord
+
+
