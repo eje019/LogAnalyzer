0ab717d68bac6b5438b59bfc459e1f3a1cf8c575
eje019 <emricndayake@gmail.com>
2026-03-22 18:23:35 +0100

    Fix : deplacement du args parse apres recuperation des entrees utilisateurs pour lire a la fois le niveau et le dossier

--- analyzer.py
+++ analyzer.py
@@ -5,10 +5,12 @@ print("Bonjour je suis l'analyseur de LOGS")
 
 analyseur = argparse.ArgumentParser(description="Analyse des fichiers de logs")
 analyseur.add_argument("--source")
-args = analyseur.parse_args()
+
 analyseur.add_argument("--niveau", default="ALL")
+
+args = analyseur.parse_args()
 
 # print("Le dossier a analyser est :", args.source, " au niveau :" ,args.niveau ,required=True)
 
 print("Dossier : ", args.source)
-print("Dossier : ", args.niveau)
+print("Niveau : ", args.niveau)
